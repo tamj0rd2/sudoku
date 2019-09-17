67f2beebcccccb6203835a1610d55a47efe9ad5b
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2019-09-17 16:16:03 +0100

    Keyboard navigation of board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,28 +1,70 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import Cell from './Cell'
 
-const getCellId = (row: number, col: number) => `${row},${col}`
+interface Coordinate {
+  col: number
+  row: number
+}
+
+const getCellId = (col: number, row: number) => `${col},${row}`
+const isValidIndex = (num: number) => num > 0 && num <= 9
+
+const coordinateReducer = (current: Coordinate, action: string | Coordinate): Coordinate => {
+  if (action && typeof action === 'string') {
+    switch (action) {
+      case 'ArrowUp': {
+        const nextRow = current.row - 1
+        return isValidIndex(nextRow) ? { col: current.col, row: nextRow } : current
+      }
+      case 'ArrowRight': {
+        const nextCol = current.col + 1
+        return isValidIndex(nextCol) ? { col: nextCol, row: current.row } : current
+      }
+      case 'ArrowDown': {
+        const nextRow = current.row + 1
+        return isValidIndex(nextRow) ? { col: current.col, row: nextRow } : current
+      }
+      case 'ArrowLeft': {
+        const nextCol = current.col - 1
+        return isValidIndex(nextCol) ? { col: nextCol, row: current.row } : current
+      }
+      default:
+        return current
+    }
+  }
+
+  if (action) {
+    return action as Coordinate
+  }
+
+  return current
+}
 
 export default function Board(): JSX.Element {
-  const [selectedCell, setSelectedCell] = useState('')
+  // const [selectedCell, setSelectedCell] = useState<Coordinate>({ row: 0, col: 0 })
+  const [selectedCell, dispatch] = useReducer(coordinateReducer, { col: 0, row: 0 })
 
   return (
-    <div>
-      {Array.from({ length: 9 }).map((_, col) => (
-        <div key={col}>
-          {Array.from({ length: 9 }).map((_, row) => {
-            const cellId = getCellId(row, col)
+    <div tabIndex={0} onKeyDown={e => dispatch(e.key)}>
+      {Array.from({ length: 9 }).map((_, y) => {
+        const row = y + 1
+        return (
+          <div key={row}>
+            {Array.from({ length: 9 }).map((_, x) => {
+              const col = x + 1
+              const cellId = getCellId(col, row)
 
-            return (
-              <Cell
-                select={() => setSelectedCell(cellId)}
-                isSelected={selectedCell === cellId}
-                key={cellId}
-              />
-            )
-          })}
-        </div>
-      ))}
+              return (
+                <Cell
+                  select={() => dispatch({ col, row })}
+                  isSelected={selectedCell.col === col && selectedCell.row === row}
+                  key={cellId}
+                />
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   )
 }
