00f4be265333f02bca42577843fa28b17b58aeab
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2019-09-17 14:19:13 +0100

    Select a single RO cell

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,28 @@
-import React from 'react'
-import Square from './Cell'
+import React, { useState } from 'react'
+import Cell from './Cell'
+
+const getCellId = (row: number, col: number) => `${row},${col}`
 
 export default function Board(): JSX.Element {
-  return <Square />
+  const [selectedCell, setSelectedCell] = useState('')
+
+  return (
+    <div>
+      {Array.from({ length: 9 }).map((_, col) => (
+        <div key={col}>
+          {Array.from({ length: 9 }).map((_, row) => {
+            const cellId = getCellId(row, col)
+
+            return (
+              <Cell
+                select={() => setSelectedCell(cellId)}
+                isSelected={selectedCell === cellId}
+                key={cellId}
+              />
+            )
+          })}
+        </div>
+      ))}
+    </div>
+  )
 }
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -2,19 +2,29 @@ import React from 'react'
 import Answer from './Answer'
 import PencilMarks from './PencilMarks'
 
+interface CellProps {
+  isSelected: boolean
+  select: () => void
+}
+
 // TODO: use styled components
-export default function Cell(): JSX.Element {
+export default function Cell({ isSelected, select }: CellProps): JSX.Element {
   const dimen = '30px'
 
   const styles: React.CSSProperties = {
     border: '1px solid black',
+    display: 'inline-block',
     width: dimen,
     height: dimen,
     position: 'relative',
   }
 
+  if (isSelected) {
+    styles.backgroundColor = 'cyan'
+  }
+
   return (
-    <div style={styles}>
+    <div style={styles} onClick={select}>
       <Answer num="" />
       <PencilMarks />
     </div>
